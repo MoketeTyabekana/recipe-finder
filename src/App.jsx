5a6443fb255ae2699b7f6da31734bf78103e4b8c
Mokete <emtyabekana@gmail.com>
2024-11-10 12:43:22 +0200

    Created a Routes for navigation in the App.js file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
-import './App.css'
+import './App.css';
+import React from 'react';
+import { BrowserRouter  as Router, Route, Routes, Link} from 'react-router-dom'; 
+import	Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Router>
+      <nav >
+        <Link to='/'Home>Home</Link>|<Link to='/about' element ={<About/>}>About</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
   )
 }
 
